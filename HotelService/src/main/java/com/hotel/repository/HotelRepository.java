b4477fca7a1bd61b6196a22e2f85c96733551f83
suresh170411 <suresh.20fk@gmail.com>
2023-08-08 22:06:25 +0530

    Hotel service jpa repository

--- HotelService/src/main/java/com/hotel/repository/HotelRepository.java
+++ HotelService/src/main/java/com/hotel/repository/HotelRepository.java
@@ -1,5 +1,7 @@
 package com.hotel.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import com.hotel.model.Hotel;
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, String> {
 
+    public Optional<Hotel> findByName(String name);
+
+    public Optional<Hotel> findByLocation(String location);
 }
